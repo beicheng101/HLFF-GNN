6c70ed9d46ae0181231cc0b4ec0816e462dc185d
beicheng101 <155811809+beicheng101@users.noreply.github.com>
2026-02-03 20:43:32 +0800

    Update hps.py

--- hps.py
+++ hps.py
@@ -1,86 +1,85 @@
 hp_cora = {
     "wd1": 4e-2,
     "wd2": 2e-5,
-    "lambda_1": 2.8,
-    "lambda_2": 0.2,
+    "lambda_1": 2.2,
+    "lambda_2": 1.2,
     "lambda_3": 0.1,
     "layer": 2,
     "dropout": 0.35,
     "lr": 0.02,
     "alpha": 0.8,
-}   # 8875 --9069   --9088
-
+}   # 88.75--90.69
 hp_pubmed = {
     "wd1": 4e-2,
     "wd2": 2e-5,
-    "lambda_1": 3.2,
-    "lambda_2": 1.8,
-    "lambda_3": 0.1,
+    "lambda_1": 2.0,
+    "lambda_2": 1.0,
+    "lambda_3": 0.5,
     "layer": 2,
     "dropout": 0.35,
-    "lr": 0.03,
+    "lr": 0.02,
     "alpha": 0.8,
-}   # 8813(9005)
-
+}   # 8813--85
 hp_citeseer = {
-    "wd1": 4e-2,
-    "wd2": 5e-5,
-    "lambda_1": 2.0,
-    "lambda_2": 1.2,
-    "lambda_3": 0.15,
+    "wd1": 4e-2,    # 5e-2 - 4e-2
+    "wd2": 5e-5,    # 2e-4 - 5e-5
+    "lambda_1": 1.0,
+    "lambda_2": 1.0,
+    "lambda_3": 0.1,
     "layer": 2,
-    "dropout": 0.3, #0.3
+    "dropout": 0.2,
     "lr": 0.015,
     "alpha": 0.8,
-}   # 76.70 --7779 (lower than 7799)  --7735
+}   # 76.70 --7779 (lower than 7799)
 
 hp_chameleon = {
-    "wd1": 2e-4,
+    "wd1": 8e-2,
     "wd2": 0.0,
-    "lambda_1": 3.5,
-    "lambda_2": 4.0,
+    "lambda_1": 1.0,
+    "lambda_2": 3.0,
     "lambda_3": 0.01,
     "layer": 2,
     "dropout": 0.25,
     "lr": 0.02,
     "alpha": 0.2,
-}   # 7463  -7778   --7843
+}   # 7463 --7734  --7865
 
 hp_squirrel = {
     "wd1": 0.075,
     "wd2": 0.0,
-    "lambda_1": 1.2,
-    "lambda_2": 1.1,    #1.2(1.3) 6449
+    "lambda_1": 1.0,
+    "lambda_2": 1.2,
     "lambda_3": 0.01,
     "layer": 2,
-    "dropout": 0.25,
+    "dropout": 0.1,
     "lr": 0.02,
     "alpha": 0.3,
-}   # 6141 --6411   --6478
+}   # 6141 -- 6372 --6411
 
 hp_amazon_computers = {
     "wd1": 5e-4,
     "wd2": 0.0,
-    "lambda_1": 0.5,
+    "lambda_1": 3.5,
     "lambda_2": 1.5,
     "lambda_3": 0.2,
     "layer": 2,
-    "dropout": 0.02,
-    "lr": 0.03,
-    "alpha": 0.85,
-}   # 8960 --9062 --9076
+    "dropout": 0.05,
+    "lr": 0.05,
+    "alpha": 0.7,
+}   # 8960 --9062--lamb2=0   9062
 
 hp_amazon_photo = {
     "wd1": 6e-4,
     "wd2": 0.0,
-    "lambda_1": 2.0,
+    "lambda_1": 2.9,
     "lambda_2": 1.4,
-    "lambda_3": 0.15,
+    "lambda_3": 0.1,
     "layer": 2,
     "dropout": 0.01,
     "lr": 0.05,
-    "alpha": 0.9,
-}   #9317 --9297  -- 9297
+    "alpha": 0.75,
+}   #9317 --9278 --lamb2=0  9291
+
 
 def get_hyper_param(name: str):
     name = name.lower()
